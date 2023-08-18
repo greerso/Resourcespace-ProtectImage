0e0178587db6429cacac8600b5f2982ac5a49a71
Greer <greer@greerso.com>
2023-08-18 15:20:24 +0000

    Added check for active session before attempting session destruction.

--- pages/js_disabled.php
+++ pages/js_disabled.php
@@ -2,8 +2,10 @@
 include "../../../include/db.php";
 include "../../../include/authenticate.php"; 
 
-// Logout the user
-session_destroy();
+// Only destroy the session if it's already started
+if (session_status() == PHP_SESSION_ACTIVE) {
+    session_destroy();
+}
 
 // Redirect to the main login page with an error message
 header("Location: " . $baseurl . "/login.php?error=JavaScript is required to use this platform.");
